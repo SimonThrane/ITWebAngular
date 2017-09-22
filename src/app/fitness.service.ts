8bbe5f87f733e956afcfd8b5a8ead6bcece03504
SimonThrane <minijordan23-virum@hotmail.com>
2017-09-22 12:15:53 +0200

    Update fitness.service.ts

--- src/app/fitness.service.ts
+++ src/app/fitness.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import {Exercise} from '../domain/exercise';
+import {Program} from '../domain/program';
 import {environment} from '../environments/environment';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class FitnessService {
@@ -9,8 +11,55 @@ export class FitnessService {
   private exerciseUrl= '/exercises';
   private programsUrl= '/programs';
   
-  getExercise(id): Exercise{
-      this.http.get(this.baseUrl + this.exerciseUrl)
+  getExercise(id: number): Promise<Exercise> {
+    const url = `${this.baseUrl + this.exerciseUrl}/${id}`;
+    return this.http.get(url)
+      .toPromise()
+      .then(response => response.json().data as Exercise)
+      .catch(this.handleError);
+  }
+
+  deleteProgram(id: number): Promise<void> {
+    const url = `${this.baseUrl + this.programsUrl}/${id}`;
+    return this.http.delete(url, {headers: this.headers})
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError);
+  }
+
+  deleteExercise(id: number): Promise<void> {
+    const url = `${this.baseUrl + this.exerciseUrl}/${id}`;
+    return this.http.delete(url, {headers: this.headers})
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError);
+  }
+
+  getPrograms(): Promise<Program[]> {
+    return this.http.get(this.baseUrl + this.programsUrl)
+               .toPromise()
+               .then(response => response.json().data as Program[])
+               .catch(this.handleError);
+  }
+
+  getProgram(id: number): Promise<Program> {
+    const url = `${this.baseUrl + this.programsUrl}/${id}`;
+    return this.http.get(url)
+      .toPromise()
+      .then(response => response.json().data as Program)
+      .catch(this.handleError);
+  }
+
+  getExercises(): Promise<Exercise[]> {
+    return this.http.get(this.baseUrl + this.exerciseUrl)
+               .toPromise()
+               .then(response => response.json().data as Exercise[])
+               .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
   }
   constructor(private http: Http) { 
     if(environment.production){
